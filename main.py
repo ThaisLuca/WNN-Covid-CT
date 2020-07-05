c5eda8f44ccc43fafa5b0e5434d2f54ef4a64dd9
thaisluca <thaisluca@uol.com.br>
2020-07-05 15:00:37 -0300

    using opencv binarization and fixing pre-processing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import sys, os
 def network(X, y, addressSize, ignoreZero=False, verbose=True):
 
 	#Define model
-	wsd = wp.Wisard(addressSize=addressSize, ignoreZero=ignoreZero, verbose=verbose)
+	wsd = wp.Wisard(addressSize, ignoreZero=ignoreZero, verbose=verbose)
 
 	print("Training\n")
 	# train using the input data
@@ -21,6 +21,7 @@ def network(X, y, addressSize, ignoreZero=False, verbose=True):
 
 	# classify train data
 	out = wsd.classify(X)
+	out = [str(i) for i in out]
 
 	print("Accuracy score: {}".format(accuracy_score(y, out)))
 	print("F1-Score: {}".format(f1_score(y, out)))
@@ -41,15 +42,21 @@ def main():
 	train_set = (pd.read_csv(os.getcwd() + params.PRE_PROCESSED_FOLDER_PATH + params.TRAIN_DATASET)).sample(frac=1)
 
 	X = train_set.drop(['target'], axis=1).values.tolist()
+	
+	# convert numbers to int and then to string
+	#X = [[str(int(x)) for x in row] for row in X]
+
 	Y = train_set['target'].values.tolist()
 	Y = [str(y) for y in Y]
 	del train_set
+	print(Y[0])
+	return
 
-	addressSizes = [20, 25, 30, 35, 40, 45, 50]
+	addressSizes = [20] #, 25, 30, 35, 40, 45, 50]
 
 	for addressSize in addressSizes:
 		print("Current address size is {}".format(addressSize))
-		network(addressSize)
+		network(X, Y, addressSize, False, True)
 
 if __name__ == "__main__":
 	sys.exit(main())
--- utils.py
+++ utils.py
@@ -11,11 +11,10 @@ def get_files_path(file, path):
 		paths = [os.getcwd() + path + x.strip() for x in content]
 	return paths
 
-def load_all_images_grayscale(images_path):
+def load_all_images(images_path):
 	images = []
 	for path in images_path:
-		image = cv2.resize(cv2.imread(path), params.DIM, interpolation=cv2.INTER_NEAREST)
-		images.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+		images.append(cv2.imread(path))
 	return images
 
 def apply_median_blur(images):
@@ -30,14 +29,20 @@ def apply_gaussian_blur(images):
 		blury_images.append(cv2.GaussianBlur(image, (5,5), 0))
 	return blury_images
 
-def pre_process_images(images_path):
-	#return load_all_images_grayscale(images_path)
-	return apply_gaussian_blur(apply_median_blur(load_all_images_grayscale(images_path)))
+def pre_process_images(images):
+	pp_images = []
+	n_images = len(images)
+	for image in images:
+		image = cv2.cvtColor(cv2.resize(image, params.DIM, interpolation=cv2.INTER_NEAREST), cv2.COLOR_BGR2GRAY)
+		ret, thresh1 = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+		pp_images.append(thresh1)	
+	return pp_images
 
-def apply_threshold(df, threshold):
+def apply_threshold(df):
 	columns = df.columns
 	for column in columns:
-		df[column] = np.where(df[column] >= threshold, 1, 0)
+		if(column == 'target'): continue
+		df[column] = np.where(df[column] == 255, '1', '0')
 	return df
 
 def create_row(array, label, keys):
@@ -52,16 +57,16 @@ def save_images_as_csv(images, n_true_labels, path, filename):
 	columns.append('target')
 
 	data = pd.DataFrame([], columns=columns)
-	label = 1
+	label = '1'
 	for image in images:
 		img_array = (image.flatten())
 		img_array = img_array.reshape(-1,1).T
 		row = create_row(img_array[0], label, columns)
 		data = data.append(row, ignore_index=True)
 
-		if(data.shape[0] == n_true_labels): label = 0 
+		if(data.shape[0] == n_true_labels): label = '0'
 
-	data = apply_threshold(data, params.THRESHOLD)
+	apply_threshold(data)
 	data.to_csv(os.getcwd() + path + filename, index=False)
 	print('Images saved as CSV\n')
 
@@ -73,9 +78,10 @@ def create_csv_files():
 	n_true_values = len(train_set)
 	
 	train_set += get_files_path(file=params.NON_COVID_TRAINING_DATASET, path=params.NON_COVID_IMAGES_PROCESSED_PATH)
+	images = load_all_images(train_set)
 	print('Pre-processing all images..')
-	images_processed = pre_process_images(train_set)
-	print("{} images processed".format(len(images_processed)))
+	images_processed = pre_process_images(images)
+	print("{} images processed".format(images_processed))
 	print('Saving images..')
 	save_images_as_csv(images_processed, n_true_values, path=params.PRE_PROCESSED_FOLDER_PATH, filename=params.TRAIN_DATASET) 
 
@@ -85,8 +91,9 @@ def create_csv_files():
 	n_true_values = len(val_set)
 
 	val_set += get_files_path(file=params.NON_COVID_VALIDATION_DATASET, path=params.NON_COVID_IMAGES_PROCESSED_PATH)
+	images = load_all_images(val_set)
 	print('Pre-processing all images..')
-	images_processed = pre_process_images(val_set)
+	images_processed = pre_process_images(images)
 	print("{} images processed".format(len(images_processed)))
 	print('Saving images..')
 	save_images_as_csv(images_processed, n_true_values, path=params.PRE_PROCESSED_FOLDER_PATH, filename=params.VALIDATION_DATASET)
@@ -97,20 +104,25 @@ def create_csv_files():
 	n_true_values = len(test_set)
 
 	test_set += get_files_path(file=params.NON_COVID_TEST_DATASET, path=params.NON_COVID_IMAGES_PROCESSED_PATH)
+	images = load_all_images(test_set)
 	print('Pre-processing all images..')
-	images_processed = pre_process_images(val_set)
+	images_processed = pre_process_images(images)
 	print("{} images processed".format(len(images_processed)))
 	print('Saving images..')
 	save_images_as_csv(images_processed, n_true_values, path=params.PRE_PROCESSED_FOLDER_PATH, filename=params.TEST_DATASET)
 
-def visualize_filters(image):
-	img = cv2.imread(image)
-	median = cv2.GaussianBlur(cv2.medianBlur(img, 5), (5,5), 0)
-	#compare = np.concatenate((img, median), axis=1)
-	median = cv2.resize(median, params.DIM, interpolation=cv2.INTER_NEAREST)
+def visualize_filters(image_path):
+	img = cv2.imread(image_path)
 
-	cv2.imshow('img', median)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	ret, thresh1 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+	ret, thresh2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	compare = np.concatenate((thresh1, thresh2), axis=1)
+
+	cv2.imshow('img', compare)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows
 
 	return
+
+#visualize_filters('foto.png')
